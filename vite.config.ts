4dbcff678d593fbecfcc7982242a5296ced47b27
Patrick Da Silva <patrick1dasilva@gmail.com>
2025-01-23 17:38:01 +0200

    Simplified setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import basicSsl from "@vitejs/plugin-basic-ssl"
+import svgrPlugin from "vite-plugin-svgr"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => ({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  plugins: [
+    react(),
+    svgrPlugin({
+      include: "**/*.svg",
+      exclude: "",
+    }),
+    ...(command === "serve" ? [basicSsl()] : []),
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5174,
+  },
+  build: {
+    target: "esnext"
+  }
+}))
