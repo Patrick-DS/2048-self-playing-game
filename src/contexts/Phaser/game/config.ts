500e8e7c1b1713fd6a25e88402cf1a815b29f95f
Patrick Da Silva <patrick1dasilva@gmail.com>
2025-01-23 22:53:31 +0200

    Switched render type to canvas

--- src/contexts/Phaser/game/config.ts
+++ src/contexts/Phaser/game/config.ts
@@ -2,10 +2,12 @@ import { MainScene } from './mainScene';
 import { Types } from "phaser"
 
 export const gameConfig: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
     width: 800,
     height: 800,
     backgroundColor: '#333333',
     transparent: true,
     scene: MainScene,
+    // Do not specify parent property
+    // We take care of this in src/contexts/Phaser/provider.tsx
 };
