44a7e6d7a05187bb272bf754a1a7ed47f20beed3
Patrick Da Silva <patrick1dasilva@gmail.com>
2025-01-23 22:56:09 +0200

    Formatted mainScene.ts and simplified adding sprite

--- src/contexts/Phaser/game/mainScene.ts
+++ src/contexts/Phaser/game/mainScene.ts
@@ -1,24 +1,23 @@
 import { GameState } from "@/types"
 
 export class MainScene extends Phaser.Scene {
-    private updateGameState?: (newState: Partial<GameState>) => void;
+  private updateGameState?: (newState: Partial<GameState>) => void
 
-    constructor() {
-        super({ key: 'MainScene' });
-    }
+  constructor() {
+    super({ key: "MainScene" })
+  }
 
-    preload() {
-        this.load.image("demo", "/demo.png")
-    }
+  preload() {
+    this.load.image("demo", "/demo.png")
+  }
 
-    init(data: { updateGameState: (newState: Partial<GameState>) => void }): void {
-        this.updateGameState = data.updateGameState;
-    }
+  init(data: { updateGameState: (newState: Partial<GameState>) => void }): void {
+    this.updateGameState = data.updateGameState
+  }
 
-    create(): void {
-        // Example of updating game state from within Phaser
-        // We just try to render demo.png
-        const sprite = this.add.sprite(0, 0, "demo");
-        sprite.setOrigin(0,0)
-    }
+  create(): void {
+    // Example of updating game state from within Phaser
+    // We just try to render demo.png
+    this.add.sprite(0, 0, "demo").setOrigin(0, 0)
+  }
 }
